8a9bf42097b10bb18787456853507a589e987523
Mingxi <65392866+mxcaoalina@users.noreply.github.com>
2026-02-22 19:56:11 -0800

    Feat: Complete checkout logic with RMQ publisher confirms

--- shoppingcart-service/src/main/java/com/cs6650/shoppingcartservice/RmqConfig.java
+++ shoppingcart-service/src/main/java/com/cs6650/shoppingcartservice/RmqConfig.java
@@ -1,9 +1,13 @@
 package com.cs6650.shoppingcartservice;
 
+import java.beans.BeanProperty;
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 
 @Configuration
 public class RmqConfig {
@@ -11,5 +15,11 @@ public class RmqConfig {
   @Bean
   public Queue shipQueue(@Value("${ship.queue:ship-order}") String name) {
     return new Queue(name, true);
+  }
+
+  // Update: Convert message to JSON for RabbitMQ
+  @Bean
+  public MessageConverter jsonMessageConverter() {
+    return new Jackson2JsonMessageConverter();
   }
 }
